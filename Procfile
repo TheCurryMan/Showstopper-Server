980e57a6114c44a1c4ac019b9c558226e5531c95
Vinay <vinayksk@gmail.com>
2018-01-20 22:26:09 -0800

    this better work

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn SpeechAI:app
+web: gunicorn router:app
--- SpeechAI.py
+++ SpeechAI.py
@@ -15,10 +15,6 @@ firebase = firebase.FirebaseApplication('https://showstoppercruz.firebaseio.com'
 def hello():
     return 'Hello World!'
 
-@app.route('/findhottest')
-def hottest():
-    return findhottest.find_pic(findhottest.get())
-
 
 if __name__ == '__main__':
     app.run()
