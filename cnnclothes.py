8bacf5912f85cbf8092e4d2dbb42dcd852fd955b
Vinay <vinayksk@gmail.com>
2018-01-21 01:48:05 -0800

    LLL

--- cnnclothes.py
+++ cnnclothes.py
@@ -73,7 +73,7 @@ def createModel():
 
 
 def predict(img):
-    test_model = load_model('1000samp.h5')
+    test_model = load_model('10000samp.h5')
     test_model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
     img = img.convert('L')
     img = resizeimage.resize_cover(img, [28,28])
