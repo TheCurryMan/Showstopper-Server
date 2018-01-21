0689c7938b61b8b2b20eec4a0abfb0ab18551180
Vinay <vinayksk@gmail.com>
2018-01-21 03:30:45 -0800

    AIDS

--- colordetect.py
+++ colordetect.py
@@ -23,7 +23,7 @@ def explicit():
     from google.cloud import storage
     # Explicitly use service account credentials by specifying the private key
     # file.
-    storage_client = storage.Client.from_service_account_json('creds.json')
+    storage_client = storage.Client.from_service_account_json('lit.json')
     # Make an authenticated API request
     buckets = list(storage_client.list_buckets())
     print(buckets)
