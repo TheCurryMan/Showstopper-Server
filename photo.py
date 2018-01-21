a73662a23cf76d6cf72d61754d9fb4e5973d5932
Ashwin Nathan <ashwinnathan@dhcp-169-233-139-209.cruznet.ucsc.edu>
2018-01-21 00:18:20 -0800

    output fx

--- photo.py
+++ photo.py
@@ -8,52 +8,52 @@ import pyrebase
 items = ["top", "bot", "top", "top", "top", "sho", "top", "sho", "bot", "sho"] 
 classes = ["t-shirt", "trouser", "pullover", "dress", "coat", "sandal", "shirt", "sneaker", "pants", "high-top"]
 
-userId = '46Hv3U0JRiZPVfaDPxHlyF4PX5g1'
-itemId = 'jewishoven'
-description = 'very moist'
+def outputs(userId, itemId):	
 
-config = {
-    "apiKey": "AIzaSyC99vx1nebCVnwmF4AhC1iAtBrKo-JpRSM",
-    "authDomain": "showstoppercruz.firebaseapp.com",
-    "databaseURL": "https://showstoppercruz.firebaseio.com",
-    "storageBucket": "showstoppercruz.appspot.com"
-}
+	description = 'very moist'
 
-firebase = pyrebase.initialize_app(config)
+	config = {
+	    "apiKey": "AIzaSyC99vx1nebCVnwmF4AhC1iAtBrKo-JpRSM",
+	    "authDomain": "showstoppercruz.firebaseapp.com",
+	    "databaseURL": "https://showstoppercruz.firebaseio.com",
+	    "storageBucket": "showstoppercruz.appspot.com"
+	}
 
-storage = firebase.storage()
-db = firebase.database()
-storage.child(userId + "/" + itemId + ".jpeg").download("test.jpeg")
-img = Image.open('test.jpeg')
+	firebase = pyrebase.initialize_app(config)
 
-#retrieve type
-cat = items[predict(img)]
+	storage = firebase.storage()
+	db = firebase.database()
+	storage.child(userId + "/" + itemId + ".jpeg").download("test.jpeg")
+	img = Image.open('test.jpeg')
 
-img1 = Image.open('test.jpeg')
-#retrieve color
-color = clothes_color("/Users/ashwinnathan/test.jpeg")
+	#retrieve type
+	cat = items[predict(img)]
 
-#retrieve class
-tag = classes[predict(img1)]
-print(cat)
-print(color)
-print(tag)
+	img1 = Image.open('test.jpeg')
+	#retrieve color
+	color = clothes_color("/Users/ashwinnathan/test.jpeg")
 
-
-subcloset = db.child("users").child(userId).child("closet").child(cat).get()
-newar = []
-if subcloset is None:
-	print('mike hunt')
-	newar = [itemId]
-else:
-	newar = subcloset.val()
-	newar.append(itemId);
-
-print(newar)
-db.child("users").child(userId).child("closet").child(cat).set(newar)
+	#retrieve class
+	tag = classes[predict(img1)]
+	print(cat)
+	print(color)
+	print(tag)
 
 
+	subcloset = db.child("users").child(userId).child("closet").child(cat).get()
+	newar = []
+	if subcloset is None:
+		print('mike hunt')
+		newar = [itemId]
+	else:
+		newar = subcloset.val()
+		newar.append(itemId);
 
-item = {'cat': cat, 'color': color, 'tag': tag, 'description': description}
+	print(newar)
+	db.child("users").child(userId).child("closet").child(cat).set(newar)
 
-db.child("items").child(itemId).set(item)
+
+
+	item = {'cat': cat, 'color': color, 'tag': tag, 'description': description}
+
+	db.child("items").child(itemId).set(item)
