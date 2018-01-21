6518a080e05ca9697699fe22ddf6a7a57f8aad04
Vinay <vinayksk@gmail.com>
2018-01-21 02:37:10 -0800

    google

--- colordetect.py
+++ colordetect.py
@@ -3,6 +3,7 @@ import os
 import webcolors as wb
 from google.cloud import vision
 from google.cloud.vision import types
+from google.cloud import storage
 
 
 common_colors = {"#FFFFFF":"white",
@@ -18,7 +19,15 @@ common_colors = {"#FFFFFF":"white",
 				 "#DEBD8B":"tan"
 				 } 
 
+def explicit():
+    # Explicitly use service account credentials by specifying the private key
+    # file.
+    storage_client = storage.Client.from_service_account_json('creds.json')
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
 
+explicit()
 
 def closest_colour(requested_colour):
     min_colours = {}
